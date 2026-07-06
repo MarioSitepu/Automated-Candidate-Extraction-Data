396cc9d07baceb142f3f3684022720d09039c2bb
MarioSitepu <stinart123@gmail.com>
2026-07-06 17:03:05 +0700

    feat : adding prisma client module, and adapter module for reading db on next, integrating auth into login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,23 +3,36 @@
 "use client"; // untuk interaksi klik
 
 import { useRouter } from "next/navigation";
-
+import { useState } from "react";
+import { loginUser } from "../actions/auth"; //memanggil jembatan backend
 
 export default function LoginPage() {
     const router = useRouter(); // alat untuk pindah halaman
 
-    //logika ke halaman dahboard
+    //1. menyimpan "memori sementara" ketikan user
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    //2. fungsi membutuhkan waktu (async) menunggu jawaban dari db
     //fungsi dijalankan saat tombol ditekan
-    const handleLogin = (e: React.FormEvent) => {
+
+    const handleLogin = async (e: React.FormEvent) =>{
         e.preventDefault(); // mencegah browser reaload halaman
-    
 
-    alert("Login berhasil ! Sedang Menuju Ke Dashboard");//popup kecil
-    router.push("/dashboard"); // ke dashboard
+        //3. Mengirim email dan password ke backend
+        const hasil = await loginUser(email, password);
 
+        //4. cek jawaban dari backend
+        //kalau benar
+        if (hasil.success === true){
+        alert(hasil.message); //munculkan popup sesuai dari action/auth.ts
+        router.push("/dashboard");
+        //kalau salah
+        } else {
+            alert(hasil.message);
+        }
     }
-    
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
@@ -33,6 +46,11 @@ export default function LoginPage() {
                             type="email"
                             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Masukkan email"
+                            required
+
+                            // sambungkan isi ketikan ke "memori sementara" email
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
 
@@ -43,6 +61,10 @@ export default function LoginPage() {
                             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Masukkan password"
                             required
+
+                            // sambungkan isi ketikan ke "memori sementara" password
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
 
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const prisma = new PrismaClient()
+
+// gajadi buat ini, karena di prisma 7 cukup url database saja ke adapter
+// 1. membuka file dev.db dengan mesin better-sqlite3
+// const sqlite = new Database('./dev.db')
+
+//2. membuat mesin menjadi adapter yg dipahami prisma
+const adapter = new PrismaBetterSqlite3({
+    url: './dev.db'
+})
+
+//3. memberikan adapter ke kuris prisma( prisma client )
+const prisma = new PrismaClient({ adapter })
+
 export default prisma
